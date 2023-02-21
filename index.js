fa1be5d8745007743a82ed0c2f636b4158671c47
Hyedryn <quentin124@hotmail.be>
2023-02-21 17:25:30 -0500

    Added multi-user support

--- index.js
+++ index.js
@@ -2,10 +2,12 @@
 import { hasListeners$, notifyIfChanged } from "./lib/notifier.js";
 import { consoleLogin } from "./lib/console-login.js";
 import { pollFavoriteBusinesses$ } from "./lib/poller.js";
-import { editConfig, resetConfig, configPath, config } from "./lib/config.js";
+import { editConfig, resetConfig, configPath, config, defaults } from "./lib/config.js";
 import yargs from "yargs";
 import { hideBin } from "yargs/helpers";
 import { createTelegramBot } from "./lib/telegram-bot.js";
+
+import {v1 as uuidv1} from 'uuid';
 
 const argv = yargs(hideBin(process.argv))
   .usage("Usage: toogoodtogo-watcher <command>")
@@ -16,8 +18,9 @@ const argv = yargs(hideBin(process.argv))
   .command("login", "Interactively login via a login email.", {
     email: {
       type: "string",
+      demandOption: true,
       describe:
-        "The email address to login with. If not specified the configured email address will be used.",
+        "The email address to login with.",
     },
   })
   .command("watch", "Watch your favourite businesses for changes.", {
@@ -43,10 +46,25 @@ switch (argv._[0]) {
     break;
 
   case "login":
+    let emailUUID;
     if (argv.email) {
-      config.set("api.credentials.email", argv.email);
+      let uuidDic = config.get("email");
+      let idx = Object.values(uuidDic).indexOf(argv.email);
+      if (idx > -1 ) {
+        emailUUID = Object.keys(uuidDic)[idx];
+      }else{
+        emailUUID = uuidv1()
+        let mailUUID = config.get('email')
+        mailUUID[emailUUID] = argv.email
+        config.set(`email`, mailUUID)
+        config.set(`${emailUUID}`, defaults["uuid"])
+        config.set(`${emailUUID}.api.credentials.email`, argv.email);
+      }
+    }else{
+      console.error("Did not get an email addr. See usage for more details.");
+      break;
     }
-    await consoleLogin();
+    await consoleLogin(emailUUID);
     break;
 
   case "watch":
@@ -55,10 +73,16 @@ switch (argv._[0]) {
       config.set(customConfig);
     }
 
-    await createTelegramBot();
-    pollFavoriteBusinesses$(hasListeners$()).subscribe({
-      next: (businesses) => notifyIfChanged(businesses),
-      error: console.error,
-    });
+
+    let dicEmail = config.get("email");
+
+    for (let emailUUID in dicEmail){
+      await createTelegramBot(emailUUID);
+      pollFavoriteBusinesses$(hasListeners$(emailUUID), emailUUID).subscribe({
+        next: (businesses) => notifyIfChanged(businesses, emailUUID),
+        error: console.error,
+      });
+      console.log("DONE");
+    }
     break;
 }
